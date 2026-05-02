f38d62362fbee2121b59befbd0fa02399694fc31
Cássio Capucho <cassiocpecanha@gmail.com>
2026-05-02 20:37:49 +0000

    Preparando conflito

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    printf("Atualização de Branche! V3\n");
-    printf("Mais uma linha\n");
+    printf("Conteudo da Linha 0\n");
+    printf("Conteudo da Linha 1\n");
+    printf("Conteudo da Linha 2\n");
+    printf("Conteudo da Linha 3\n");
+    printf("Conteudo da Linha 4\n");
+    printf("Conteudo da Linha 5\n");
+    printf("Conteudo da Linha 6\n");
+    printf("Conteudo da Linha 7\n");
+    printf("Conteudo da Linha 8\n");
+    printf("Conteudo da Linha 9\n");
     return 0;
 
 
